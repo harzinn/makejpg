288be5c2f5bf5a5e9da169a89223719e49387c90
harzinn <nickkrugz@gmail.com>
2021-11-18 20:10:30 -0500

    updated dev build

--- makejpgdev.c
+++ makejpgdev.c
@@ -1,3 +1,9 @@
+/* makejpg_v0.1
+ * Create random grayscale images for testing
+ * 18 Nov 2021
+ * Harzinn
+*/
+
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
@@ -5,24 +11,24 @@
 int main(void)
 {
 	int b;
-	char buf[100];
-	srand(time(NULL));
+	char buf[100]; // buffer required for system call later
+	srand(time(NULL)); // randomizer NOW outside of loop!...whoops
 
-	printf("How many images do you want to create?\n");
+	printf("\nHow many images do you want to create?\n");
 	scanf("%d", &b);
+	printf("\n");
 
 	for (;b>0;b--)
 	{
 		unsigned int a = rand();
 
-		sprintf(buf, "convert -size 4000x4000 xc:gray +noise gaussian %x.jpg\n", a/2);
-		printf("Creating random JPEG image %x.jpg\n", a/2);
+		sprintf(buf, "convert -size 4000x4000 xc:gray +noise gaussian %x.jpg\n", a);
+		printf("Creating random JPEG image %x.jpg\n", a);
 		system(buf);
-		printf("%x.jpg created\n", a/2);
 	}
 
 	printf("\nCalculating sha512 hash\n");
 	system("sha512sum *.jpg > sha512index");
-	printf("\n\nsha512 values stored as sha512index\n\n");
+	printf("sha512 values stored as sha512index\n\n");
 	return 0;
 }
